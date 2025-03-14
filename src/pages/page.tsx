a6424c04baecb621fb3a2e5e7544f11366ffb6b1
wildthinkslaboratory <goldfishandrobin@gmail.com>
2025-03-13 19:00:54 -0700

    cleaning up css file

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -6,26 +6,22 @@ import { TEACHING_ITEMS } from "../components/resume/resume-data";
 
 function Page() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="main-page">
-          <div className="main-vertical-header">
-            <h1>Heidi Dixon</h1>
-            <h3 className="identity">
-              creative problem solver, programmer, teacher.
-            </h3>
-          </div>
-          <div className="main-vertical-scroller">
-            <article className="resume-item-page">
-              <ResumeList items={PROJECT_ITEMS} />
-            </article>
-            <article className="resume-item-page">
-              <ResumeList items={TEACHING_ITEMS} />
-            </article>
-          </div>
-        </div>
-      </header>
-    </div>
+    <main className="main-page">
+      <div className="main-vertical-header">
+        <h1>Heidi Dixon</h1>
+        <h3 className="identity">
+          creative problem solver, programmer, teacher.
+        </h3>
+      </div>
+      <div className="main-vertical-scroller">
+        <article className="resume-item-page">
+          <ResumeList items={PROJECT_ITEMS} />
+        </article>
+        <article className="resume-item-page">
+          <ResumeList items={TEACHING_ITEMS} />
+        </article>
+      </div>
+    </main>
   );
 }
 
